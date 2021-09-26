11e5b217f531c8157ae9e1942d8a468d4943c730
Grant Mathews <johnfn@Grants-MacBook-Pro.local>
2021-09-25 19:42:58 -0700

    Lowkey enemy movement

--- NPC.gd
+++ NPC.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
 onready var player: Player = $"../Player"
+var patrols: Array = []
+var current_patrol_index = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-  print(player)
-  
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+  for node in get_children():
+    var name: String = node.name
+     
+    if name.begins_with("Patrol"):
+      patrols.append(node.global_position)
 
 func _physics_process(delta):
-  print(player)
-  var movement_vec = player.position - self.position
+  var current_patrol = patrols[current_patrol_index]
+  
+  if position.distance_to(current_patrol) < 10:
+    current_patrol_index = (current_patrol_index + 1) % len(patrols)
+    current_patrol = patrols[current_patrol_index]
+  
+  var movement_vec = (current_patrol - position).normalized() * 100
   
   move_and_slide(movement_vec)
-  
