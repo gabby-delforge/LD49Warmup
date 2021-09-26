d3cbdc73e1ccef5c5b47cac85d08ca0e307be13f
Grant Mathews <johnfn@Grants-MacBook-Pro.local>
2021-09-25 20:10:10 -0700

    Charging enemies

--- NPC.gd
+++ NPC.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 onready var player: Player = $"../Player"
 var patrols: Array = []
 var current_patrol_index = 0
+var can_see_player = false
 
 func _ready():
   for node in get_children():
@@ -12,12 +13,25 @@ func _ready():
       patrols.append(node.global_position)
 
 func _physics_process(delta):
-  var current_patrol = patrols[current_patrol_index]
+  var current_target: Vector2
   
-  if position.distance_to(current_patrol) < 10:
-    current_patrol_index = (current_patrol_index + 1) % len(patrols)
-    current_patrol = patrols[current_patrol_index]
-  
-  var movement_vec = (current_patrol - position).normalized() * 100
+  if can_see_player:
+    current_target = player.position
+  else:
+    current_target = patrols[current_patrol_index]
+    
+    if position.distance_to(current_target) < 10:
+      current_patrol_index = (current_patrol_index + 1) % len(patrols)
+      current_target = patrols[current_patrol_index]
+    
+  var movement_vec = (current_target - position).normalized() * (200 if can_see_player else 100)
   
   move_and_slide(movement_vec)
+
+func _on_Area_body_entered(body):
+  if body == player:
+    can_see_player = true
+
+func _on_Area_body_exited(body):
+  if body == player:
+    can_see_player = false
